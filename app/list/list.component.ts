b8cd3fc681c8e568306619bf97f04ef164261b9b
Wenzel Paul Trummer <wenzel.trummer@student.tugraz.at>
2024-02-29 01:21:29 +0100

    init

---
+++ app/list/list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent {
+  constructor(public ds: DataService) { }
+
+}
